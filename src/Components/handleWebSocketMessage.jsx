647823bbd81711d1d764ba59063ba8c3f4d708bf
Malpizarr <m.alpizar@hotmail.com>
2024-03-11 10:56:59 -0600

    EStructural changes

--- src/Components/handleWebSocketMessage.jsx
+++ src/Components/handleWebSocketMessage.jsx
@@ -2,7 +2,6 @@ export function handleWebSocketMessage(event, setFriends, setMessages, friendId)
     const data = JSON.parse(event.data);
 
     if (data.type === 'friendsList' && setFriends) {
-        // Asegúrate de que setFriends es una función antes de llamarla
         setFriends(friends => friends.map(friend => ({
             ...friend,
             status: data.friends.includes(friend.friendId) ? 'En línea' : 'Fuera de línea'
